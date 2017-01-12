ba6a5cb9a53e372aa1af25b8378f0a9b09224b3e
Akhil <akhil.jain077@iitkgp.ac.in>
2017-01-12 19:29:41 +0530

    Finallygit add -A Perfectly Correct

--- ASS1/xsort.c
+++ ASS1/xsort.c
@@ -15,7 +15,7 @@ int main(int argc,char *argv[]){
 	strcpy(parameters[0],"./sort1");
 	strcpy(parameters[1],argv[1]);
 	parameters[2]=NULL;
-	execvp("./sort1",parameters);
-	perror("execvp failed");
+	execlp("/usr/bin/xterm","/usr/bin/xterm","-hold","-e",parameters[0],parameters[1],(char*)NULL);
+	perror("exec failed");
 	return 0;
 }
