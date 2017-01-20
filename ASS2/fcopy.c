adc9ed182664018a54909b5bcf97b83b13ce5ca2
Akhil <akhil.jain077@iitkgp.ac.in>
2017-01-20 16:27:14 +0530

    Handled every exception

--- ASS2/fcopy.c
+++ ASS2/fcopy.c
@@ -21,36 +21,38 @@ int main(int argc, char* argv[]){
 		perror("Incorrect File1 name\n");
 		return 0;
 	}
-	filedes2=open(argv[2],O_WRONLY | O_CREAT,S_IRUSR | S_IWUSR);
+	filedes2=open(argv[2],O_TRUNC|O_WRONLY | O_CREAT,S_IRUSR | S_IWUSR);
 	if(filedes2<0){
 		perror("Incorrect File2 name\n");
 		return 0;
 	}	
 	id=fork();
-	int tmp;
+	int bytes;
 	char a[10];
 	if(id!=0){
 		close(fd[0]);
-		while(tmp=read(filedes1,buff,BUFSIZE)){
-			write(fd[1],buff,tmp);
+		while(1){
+			bytes=read(filedes1,buff,BUFSIZE);
+			write(fd[1],buff,bytes);
 			printf("Parent has written to the pipe ... waiting for Acknowledgement \n");
 			read(ret[0],a,2);
 			if(a[0]=='-' && a[1]=='1')exit(-1);
-			if(a[0]=='0' && tmp<100)exit(1);
+			if(a[0]=='0' && bytes<100)exit(1);
 		}
 
 	}
 	else{
 		close(fd[1]);
-		while(tmp=read(fd[0],buff,BUFSIZE)){
-			write(filedes2,buff,tmp);
+		while(1){
+			bytes=read(fd[0],buff,BUFSIZE);
+			write(filedes2,buff,bytes);
 			printf("Child has appended to the file\n");
-			if(tmp<0){
+			if(bytes<0){
 				write(ret[1],"-1",2);
 				perror("Error in copying\n");
 				exit(-1);	
 			}
-			else if(tmp<100){
+			else if(bytes<100){
 				write(ret[1],"0",2);
 				printf("File copied successfully\n");
 				exit(1);
