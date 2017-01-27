935c46ccb8002eceb9f148975f015e2a545dad5d
Akhil <akhil.jain077@iitkgp.ac.in>
2017-01-27 21:01:29 +0530

    Everything upto cp is correct

--- ASS2/Ass2b/shell.c
+++ ASS2/Ass2b/shell.c
@@ -6,9 +6,11 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <time.h>
+#include <fcntl.h>
 #define mx 500
-
+#define BUFSIZE 100
 char currD[500],in[500],path[mx],tmp[mx];
+
 
 
 void cd_(){
@@ -96,10 +98,82 @@ void ls_(){
 
 }
 
+void fcpy(char * argv[]){
+	int fd[2],id,filedes1,filedes2,ret[2]; //fd for reading/writing , ret for acknowledgement
+	char buff[200];
+	pipe(fd);   // pipe1
+	pipe(ret); //pipe2
+	filedes1=open(argv[1],O_RDONLY);
+	if(filedes1<0){
+		perror("Incorrect File1 name\n");
+		return;
+	}
+	filedes2=open(argv[2],O_TRUNC|O_WRONLY,S_IRUSR | S_IWUSR); /* If file not present,create. If present overwrite */
+	if(filedes2<0){
+		perror("Incorrect File2 name\n");
+		return;
+	}	
+	id=fork();
+	int bytes;
+	char a[10];
+	if(id!=0){   // parent proces
+		close(fd[0]);
+		while(1){
+			bytes=read(filedes1,buff,BUFSIZE); // reading from file 1
+			write(fd[1],buff,bytes);  // writing to pipe
+			read(ret[0],a,2);  // waiting for acknowledgement
+			if(a[0]=='-' && a[1]=='1')return;
+			if(a[0]=='0' && bytes<100)return;
+		}
+
+	}
+	else{
+		close(fd[1]);
+		while(1){
+			bytes=read(fd[0],buff,BUFSIZE); // reading from pipe
+			write(filedes2,buff,bytes); // writing to file 2
+			if(bytes<0){
+				write(ret[1],"-1",2);
+				return;	
+			}
+			else if(bytes<100){
+				write(ret[1],"0",2); // sending acknowledgement
+				return; 
+			}
+			else write(ret[1],"0",2); // sending acknowledgement
+		}
+	}
+
+}
+
+
 void cp_(){
 	char file1[mx],file2[mx];
 	char* tmp=strtok(NULL," ");
 	char* tmp2=strtok(NULL," ");
+	if(tmp==NULL || tmp2==NULL){
+		printf("cp requires two arguments\n");
+		return;
+	}
+	strcpy(file1,tmp);
+	strcpy(file2,tmp2);
+	printf("\nFile1=%s File2=%s",file1,file2);
+	struct stat filestat1,filestat2;
+	stat(file1,&filestat1);
+	stat(file2,&filestat2);
+	if(filestat1.st_mtime > filestat2.st_mtime){
+		char *argv[3];
+		argv[0]=(char *)malloc(mx*sizeof(char));
+		argv[1]=(char *)malloc(mx*sizeof(char));
+		argv[2]=(char *)malloc(mx*sizeof(char));
+		strcpy(argv[0],file1);
+		strcpy(argv[1],file1);
+		strcpy(argv[2],file2);
+		fcpy(argv);
+	}
+	else{
+		printf("Kyu\n");
+	}
 }
 
 void shell(){
