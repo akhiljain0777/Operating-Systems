45d96c22052878e0474b32f383e005f61e8b074e
Akhil <akhil.jain077@iitkgp.ac.in>
2017-02-24 21:58:46 +0530

    Handled some user exceptions

--- ASS3/x.c
+++ ASS3/x.c
@@ -7,6 +7,8 @@
 #include <sys/wait.h>
 #include <sys/sem.h>
 #include <string.h>
+#include <signal.h>
+
 
 #define P(s) semop(s, &wait_op, 1) 
 #define V(s) semop(s, &signal_op, 1)  
@@ -27,6 +29,20 @@ void update(char *filename,struct record *records,int size){
 	fclose(file);
 }
 
+int shmid,shmid2,i=0,shmid3;
+int semid1,*flag;
+struct record *records;
+key_t key=1101,key2=1100,key3=3700;	
+
+void my_handler(){
+	shmdt(flag);
+	shmdt(records);
+	semctl(semid1, 0, IPC_RMID, 0);
+	shmctl(shmid, IPC_RMID, 0);
+	shmctl(shmid2, IPC_RMID, 0);
+	shmctl(shmid3, IPC_RMID, 0);
+	exit(0);
+}
 
 
 int main(int argc,char *argv[]){
@@ -34,18 +50,14 @@ int main(int argc,char *argv[]){
 		printf("Invalid input\n" );
 		return 0;
 	}
-	int shmid,shmid2,i=0;
-	int semid1,semid2,*flag;
-	struct record *records;
-	key_t key=1101,key2=1100,key3=1102;
+	signal(SIGINT, my_handler);
+	
 	shmid=shmget(key,110*sizeof(struct record),0777|IPC_CREAT);
 	records=(struct record *)shmat(shmid,NULL,0);
 	shmid2=shmget(key2,2*sizeof(int),0777|IPC_CREAT);
 	flag=(int *)shmat(shmid2,NULL,0);
 	semid1 = semget(key3, 1, 0777|IPC_CREAT);
-	semid2 = semget(IPC_PRIVATE, 1, 0777|IPC_CREAT);
 	semctl(semid1,0,SETVAL,1);
-	semctl(semid2,0,SETVAL,1);
 	FILE *file=fopen(argv[1],"r");
 	if(file==NULL){printf("File can not opened");return 0;}
 	signal_op.sem_num=wait_op.sem_num=0;
@@ -57,10 +69,11 @@ int main(int argc,char *argv[]){
 	flag[1]=i;
 	flag[0]=0;
 	//TODO
+	shmid3 = shmget(key3,sizeof(int),0777|IPC_CREAT);
 	while(1){
 		sleep(5);
 		P(semid1);
-		if(flag[0])update(argv[1],records,flag[1]);
+		if(flag[0]){update(argv[1],records,flag[1]);}
 		flag[0]=0;
 		V(semid1);
 	}
--- ASS3/y.c
+++ ASS3/y.c
@@ -31,32 +31,58 @@ void search(struct record *records,int size){
 }
 
 
-void update(struct record *records,int size,int semid1){
-	P(semid1);
+void update(struct record *records,int size,int semid1,int *flag){
 	int rollno,i;
+	//P(semid1);
 	printf("Enter roll no\n");
 	scanf("%d",&rollno);
 	for(i=0;i<size;i++){
 		if(records[i].roll==rollno){
 			printf("Old CGPA=%.2f\nEnter new CGPA\n",records[i].cgpa);
-			scanf("%f",&records[i].cgpa);
+			float tmp;
+			scanf("%f",&tmp);
+			P(semid1);
+			records[i].cgpa=tmp;
+			*flag=1;
+			V(semid1);
 			printf("CGPA updated successfully\n");
 			break;
 		}
 	}
 	if(i==size)printf("Invalid roll no\n");
-	V(semid1);
+
+			
+}
+int *flag;
+struct record *records;
+
+void my_handler(){
+	shmdt(flag);
+	shmdt(records);
+	exit(0);
 }
 
 int main(){
-	int shmid,i,shmid2,semid1,*flag;
-	struct record *records;
-	key_t key=1101,key2=1100,key3=1102;
+	int shmid,i,shmid2,semid1,shmid3;
+	key_t key=1101,key2=1100,key3=3700;
+	signal(SIGINT,my_handler);
+	shmid3 = shmget(key3 , sizeof(int) , 0666);
+	while(shmid3 < 0){
+		printf("Waiting for x to start\n");
+		shmid3 = shmget(key3 , sizeof(int) , 0666);
+		sleep(2);
+	}
 	shmid=shmget(key,110*sizeof(struct record),0777|IPC_CREAT);
 	records=(struct record *)shmat(shmid,NULL,0);
 	shmid2=shmget(key2,2*sizeof(int),0777|IPC_CREAT);
 	flag=(int *)shmat(shmid2,NULL,0);
 	semid1 = semget(key3, 1, 0777|IPC_CREAT);
+	signal_op.sem_num=wait_op.sem_num=0;
+	wait_op.sem_op=-1;
+	wait_op.sem_flg=signal_op.sem_num=0;
+	signal_op.sem_op=1;
+	
+
 	while(1){
 		printf("1.Search for a student record\n");
 		printf("2.Update CGPA of a student\n");
@@ -64,9 +90,12 @@ int main(){
 		int n;
 		scanf("%d",&n);
 		if(n==1)search(records,flag[1]);
-		else if(n==2){update(records,flag[1],semid1);flag[0]=1;}
+		else if(n==2){update(records,flag[1],semid1,&flag[0]);}
 		else if(n==3)break;
 		else printf("Invalid input\nPlease try again\n");
 	}
+	shmdt(flag);
+	shmdt(records);
+
 	return 0;
 }
